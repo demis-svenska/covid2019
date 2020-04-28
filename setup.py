d94376de279613bac4fefa5b9b1f5a3909c37e8d
demis <mailtodemis08@gmail.com>
2020-04-28 10:06:09 +0000

    minio added instead of shared_drive

--- setup.py
+++ setup.py
@@ -30,11 +30,7 @@ setup(
     },
     install_requires=(
         'ansible-vault==1.1.1',
-<<<<<<< HEAD
         'ansible>=2.9.4',
-=======
-        'ansible>=2.8.0',
->>>>>>> demis/master
         'argparse>=1.4',
         'attrs>=18.1.0',
         'boto3>=1.9.131',
